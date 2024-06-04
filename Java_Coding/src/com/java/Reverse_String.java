cb0f258f7b3dd022136c9e2febd97b2916878ffa
user <shivakumar.github@gmail.com>
2024-06-04 12:43:20 +0530

    Reverse a String in 4 Ways

---
+++ Java_Coding/src/com/java/Reverse_String.java
@@ -0,0 +1,34 @@
+package com.java;
+
+public class Reverse_String {
+
+	public static void main(String[] args) {
+
+		String str = "Shiva kumar";
+
+		// Approach---1
+
+		char[] array = str.toCharArray();
+		for (int i = array.length - 1; i >= 0; i--) {
+			System.out.print(array[i]);
+		}
+		System.out.println("");
+
+		// Approach---2
+		for (int i = str.length() - 1; i >= 0; i--) {
+			System.out.print(str.charAt(i));
+		}
+		System.out.println("");
+
+		// Approach---3
+
+		// StringBuffer buffer = new StringBuffer(str);
+		// System.out.println(buffer.reverse());
+		// }
+
+		// Approach---4
+		StringBuilder builder = new StringBuilder(str);
+		System.out.println(builder.reverse());
+
+	}
+}
